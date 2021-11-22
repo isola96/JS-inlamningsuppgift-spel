ddc9537228ab269dae555f0119afb9bf60850bf3
Johanna Jönsson <johannajonsson96@hotmail.com>
2021-11-22 13:45:59 +0100

    added all the images and names and basic html and a working function for a random name

---
+++ app.js
@@ -0,0 +1,168 @@
+const students = [
+	{
+		name: "Adi Dzocaj",
+		image: "images/adi-dzocaj.jpg",
+	},
+	{
+		name: "Alexander Bergquist",
+		image: "images/alexander-bergquist.jpg",
+	},
+	{
+		name: "Alexander Kocman",
+		image: "images/alexander-kocman.jpg",
+	},
+	{
+		name: "Benjamin Benson",
+		image: "images/benjamin-benson.jpg",
+	},
+	{
+		name: "Benjamin Tsubarah",
+		image: "images/benjamin-tsubarah.jpg",
+	},
+	{
+		name : "Calle Nilsson",
+		image: "images/calle-nilsson.jpg",
+	},
+	{
+		name: "Chikage Takahashi Molander",
+		image: "images/chikage-takahashi-molander.jpg",
+	},
+	{
+		name: "Daniel Be",
+		image: "images/daniel-be.jpg",
+	},
+	{
+		name: "Daniel Carlsson",
+		image: "images/daniel-carlsson.jpg",
+	},
+	{
+		name: "Elin Ahlgren",
+		image: "images/elin-ahlgren.jpg",
+	},
+	{
+		name: "Emma Käck",
+		image: "images/emma-kack.jpg",
+	},
+	{
+		name: "Eric Ståhl",
+		image: "images/eric-stahl.jpg",
+	},
+	{
+		name: "Frans Gustavson Påsse",
+		image: "images/frans-gustavson-passe.jpg",
+	},
+	{
+		name: "Glafira Veretennikova",
+		image: "images/glafira-veretennikova.jpg",
+	},
+	{
+		name: "Gustaf Grönlund",
+		image: "images/gustaf-gronlund.jpg",
+	},
+	{
+		name: "Hanna Håkanson",
+		image: "images/hanna-hakanson.jpg",
+	},
+	{
+		name: "Heidi Sjöberg",
+		image: "images/heidi-sjoberg.jpg",
+	},
+	{
+		name: "Hugo Carzborn",
+		image: "images/hugo-carzborn.jpg",
+	},
+	{
+		name: "Jesper Kling",
+		image: "images/jesper-kling.jpg",
+	},
+	{
+		name: "Johan Ranestam",
+		image: "images/johan-ranestam.jpg",
+	},
+	{
+		name: "Johanna Bäckström",
+		image: "images/johanna-backstrom.jpg",
+	},
+	{
+		name: "Johanna Jönsson",
+		image: "images/johanna-jonsson.jpg",
+	},
+	{
+		name: "Jona Torsson",
+		image: "images/jona-torsson.jpg",
+	},
+	{
+		name: "Josefine Ahlstedt",
+		image: "images/josefine-ahlstedt.jpg",
+	},
+	{
+		name: "Julia Jespersdotter Högman",
+		image: "images/julia-jespersdotter-hogman.jpg",
+	},
+	{
+		name: "Julia Nemell",
+		image: "images/julia-nemell.jpg",
+	},
+	{
+		name: "Linus Lindberg",
+		image: "images/linus-lindberg.jpg",
+	},
+	{
+		name: "Malin Olsson",
+		image: "images/malin-olsson.jpg",
+	},
+	{
+		name: "Maria Haara-Lundhammar",
+		image: "images/maria-haara-lundhammar.jpg",
+	},
+	{
+		name: "Maria Lövgren",
+		image: "images/maria-lovgren.jpg",
+	},
+	{
+		name: "Nikola Dimitrijoski",
+		image: "images/nikola-dimitrijoski.jpg",
+	},
+	{
+		name: "Paulina Kiendys",
+		image: "images/paulina-kiendys.jpg",
+	},
+	{
+		name: "Raymond Lam",
+		image: "images/raymond-lam.jpg",
+	},
+	{
+		name: "Robin Karlsson",
+		image: "images/robin-karlsson.jpg",
+	},
+	{
+		name: "Sara Almqvist",
+		image: "images/sara-almqvist.jpg",
+	},
+	{
+		name: "Tim Nilsson",
+		image: "images/tim-nilsson.jpg",
+	},
+	{
+		name: "Tirapat Sukjit",
+		image: "images/tirapat-sukjit.jpg",
+	},
+	{
+		name: "Tobias Silfverberg",
+		image: "images/tobias-silfverberg.jpg",
+	},
+	{
+		name: "Wiktoria Dobrzewinska",
+		image: "images/wiktoria-dobrzewinska.jpg",
+	},
+];
+
+const showImageEl = document.querySelector('#img-classmates');
+const button1 = document.querySelector('#first-choice');
+const button2 = document.querySelector('#second-choice');
+const button3 = document.querySelector('#third-choice');
+const button4 = document.querySelector('#fourth-choice');
+
+let random = students[Math.floor(Math.random() * students.length)].name;
+console.log(random);
+
